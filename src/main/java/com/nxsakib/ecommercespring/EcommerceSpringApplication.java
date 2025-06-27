232e03b0de4c634eb35b8f0f2b431faff898fd63
Nazmus Sakib <nxsakib1@gmail.com>
2025-06-27 23:44:34 +0600

    feat: integrate dotenv for environment variable management

--- src/main/java/com/nxsakib/ecommercespring/EcommerceSpringApplication.java
+++ src/main/java/com/nxsakib/ecommercespring/EcommerceSpringApplication.java
@@ -1,5 +1,6 @@
 package com.nxsakib.ecommercespring;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,7 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcommerceSpringApplication {
 
     public static void main(String[] args) {
-
+        Dotenv dotenv = Dotenv.load();
+        dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
         SpringApplication.run(EcommerceSpringApplication.class, args);
     }
 
