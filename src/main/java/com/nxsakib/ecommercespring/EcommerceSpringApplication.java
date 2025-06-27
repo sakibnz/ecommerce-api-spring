d224bb783fb546daee5c784ea33b654e192edcd5
Nazmus Sakib <nxsakib1@gmail.com>
2025-06-27 21:38:58 +0600

    Initial Commit

---
+++ src/main/java/com/nxsakib/ecommercespring/EcommerceSpringApplication.java
@@ -0,0 +1,14 @@
+package com.nxsakib.ecommercespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceSpringApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(EcommerceSpringApplication.class, args);
+    }
+
+}
