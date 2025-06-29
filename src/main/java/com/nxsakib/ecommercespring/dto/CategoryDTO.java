83e936ff06c832e1744fa32c80dca95a8ebf851c
Nazmus Sakib <nxsakib1@gmail.com>
2025-06-29 21:17:18 +0600

    feat: add CategoryDTO

---
+++ src/main/java/com/nxsakib/ecommercespring/dto/CategoryDTO.java
@@ -0,0 +1,12 @@
+package com.nxsakib.ecommercespring.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class CategoryDTO {
+    private String name;
+}
