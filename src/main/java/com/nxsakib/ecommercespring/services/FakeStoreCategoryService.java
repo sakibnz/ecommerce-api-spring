9ed5da0e83e7d651ef0b0a07643a077ea1917eb5
Nazmus Sakib <nxsakib1@gmail.com>
2025-06-29 21:19:28 +0600

    chore: create ICategoryService interface and its FakeStore implementation

---
+++ src/main/java/com/nxsakib/ecommercespring/services/FakeStoreCategoryService.java
@@ -0,0 +1,11 @@
+package com.nxsakib.ecommercespring.services;
+
+import com.nxsakib.ecommercespring.dto.CategoryDTO;
+
+public class FakeStoreCategoryService implements ICategoryService{
+
+    @Override
+    public CategoryDTO getAllCategories() {
+        return null;
+    }
+}
---
+++ src/main/java/com/nxsakib/ecommercespring/services/ICategoryService.java
@@ -0,0 +1,7 @@
+package com.nxsakib.ecommercespring.services;
+
+import com.nxsakib.ecommercespring.dto.CategoryDTO;
+
+public interface ICategoryService {
+    CategoryDTO getAllCategories();
+}
