18dbee3547e58d491e329228252725243663a50c
Nazmus Sakib <nxsakib1@gmail.com>
2025-07-01 10:50:52 +0600

    refactor: get base url from environment variables

--- src/main/java/com/nxsakib/ecommercespring/configurations/RetrofitConfig.java
+++ src/main/java/com/nxsakib/ecommercespring/configurations/RetrofitConfig.java
@@ -1,6 +1,7 @@
 package com.nxsakib.ecommercespring.configurations;
 
 import com.nxsakib.ecommercespring.gateways.api.IFakeStoreCategoryApi;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import retrofit2.Retrofit;
@@ -8,11 +9,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 @Configuration
 public class RetrofitConfig {
+    @Value("${FAKE_STORE_BASE_URL}")
+    private String fakeStoreBaseUrl;
 
     @Bean
     public Retrofit retrofit() {
         return new Retrofit.Builder()
-                .baseUrl("https://fakestoreapi.in/api/")
+                .baseUrl(fakeStoreBaseUrl)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
