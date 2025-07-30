92eb27c49fe4ecc09d92c5c08f4dca9e148599ad
Nazmus Sakib <nxsakib1@gmail.com>
2025-07-30 10:01:51 +0600

    feat: add category entity

---
+++ src/main/java/com/nxsakib/ecommercespring/entities/Category.java
@@ -0,0 +1,24 @@
+package com.nxsakib.ecommercespring.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Category extends BaseEntity {
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
+}
+
